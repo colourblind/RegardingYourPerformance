1889ef560a579d108dc66ebca175165d9813057a
colourblind <tom.milsom@gmail.com>
2011-03-11 22:56:20 +0000

    Trying to pin down a particular error . . .

--- src/Agent/Program.cs
+++ src/Agent/Program.cs
@@ -10,6 +10,8 @@ namespace RegardingYourPerformance.Agent
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             if (args.Length > 0)
             {
                 if (args[0] == "-i" || args[0] == "-u")
@@ -45,6 +47,25 @@ namespace RegardingYourPerformance.Agent
 			        new Service() 
 		        };
                 ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            System.IO.StreamWriter writer = null;
+            Exception ex = (Exception)e.ExceptionObject; // CLI 10.5 - oh interop, you so crazy
+            try
+            {
+                writer = new System.IO.StreamWriter("c:\\ryp_crash.log", true);
+                writer.WriteLine(String.Format("{0} ________", DateTime.Now));
+                writer.WriteLine(ex.Message);
+                writer.WriteLine(ex.StackTrace);
+                writer.WriteLine();
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
             }
         }
     }
